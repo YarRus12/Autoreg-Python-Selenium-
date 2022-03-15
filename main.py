380fae1937b09f1631b65f85236445e3bdea1ea0
Yaroslav Russu <yaroslav.russu@mail.ru>
2022-03-15 17:55:05 +1000

    First step for Selentium project

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 from selenium import webdriver
+import time
 #from selenium.webdriver.common.by import By
 #from selenium.webdriver.common.keys import Keys
 
 
-name='sometestname12341234@gmail.com'
-password='sometest1234'
+class Automail:
 
-driver = webdriver.Firefox()
-driver.get('https://accounts.google.com/signin/v2/identifier')
-driver.find_element_by_id("identifierId").send_keys(name)
-driver.find_element_by_id("identifierNext").click()
-driver.find_element_by_id("password").send_keys(password)
-driver.find_element_by_id("passwordNext").click()
+    def __init__(self):
+        self.driver = driver = webdriver.Firefox()
+
+    def login(self, email, password):
+        self.driver.get('https://accounts.google.com/signin/v2/identifier')
+        self.driver.find_element_by_id("identifierId").send_keys(email)
+        self.driver.find_element_by_id("identifierNext").click()
+        time.sleep(3)
+        self.driver.find_element_by_name('password').send_keys(password)
+        self.driver.find_element_by_id("passwordNext").click()
+
+#    def mail(self):
 
 
-#driver.get("https://github.com/login")
-#driver.find_element_by_id("login_field").send_keys(name)
-#driver.find_element_by_id("password").send_keys(password)
+if __name__ == '__main__':
+    mail = Automail()
+    mail.login('sometestname12341234@gmail.com', 'sometest1234')
