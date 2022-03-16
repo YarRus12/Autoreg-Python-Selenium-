924c43e2b3bbac9cd5c62d3f14ccb7fda99fb392
YarRus12 <97214900+YarRus12@users.noreply.github.com>
2022-03-16 17:28:34 +1000

    Were written scripts for mail and password generation. Automail class rewrited for gmail box, becouse it does not contain capture. Added file with information about people for creating emails

---
+++ testing_script.py
@@ -0,0 +1,4 @@
+
+self.driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/div/div/div[2]/div/div[1]"
+                                   "/div/form/span/section/div/div/div[1]/div[1]/div[1]/div/div[1]/div/div[1]/input").send_keys(
+    name)
