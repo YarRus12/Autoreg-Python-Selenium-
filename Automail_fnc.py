86acd1fa5cc26936aa387dd8b5f1b9a37637e79a
YarRus12 <97214900+YarRus12@users.noreply.github.com>
2022-03-16 14:30:01 +1000

    Were written scripts for mail and password generation. Automail class rewrited for gmail box, becouse it does not contain capture

--- Automail_fnc.py
+++ Automail_fnc.py
@@ -8,8 +8,51 @@ class Automail:
     def __init__(self):
         self.driver = webdriver.Firefox('geckodriver.exe')
 
-    def registry(self):
+    def registry(self, name, surname, birthdate, s):
         """Зарегистрировать новую электронную почту в любом почтовом сервисе"""
+        #We choose gmail server
+        mail_registration = 'https://accounts.google.com/signup/v2/webcreateaccount?service=mail&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&flowName=GlifWebSignIn&flowEntry=SignUp'
+        # Вводим имя
+        self.driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/div/div/div[2]/div/div[1]"
+                                           "/div/form/span/section/div/div/div[1]/div[1]/div[1]/div/div[1]/div/div[1]/input").send_keys(name)
+        # Вводим фамилию
+        self.driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/div/div/div[2]/div/div[1]"
+                                           "/div/form/span/section/div/div/div[1]/div[1]/div[2]/div/div[1]/div/div[1]/input").send_keys(surname)
+        #вызываем функцию генерации адреса эл почты
+        email_address = create_mail(name,surname,birthdate)
+        #Вводим сгенерированный адрес эл. почты
+        self.driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/div/div/div[2]/div/div[1]"
+                                           "/div/form/span/section/div/div/div[2]/div[1]/div/div[1]/div/div[1]/input").send_keys(email_address)
+        #вызываем функцию генерации пароля
+        password = create_password()
+        #вводим сгенерированный пароль
+        self.driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/div/div/div[2]/div/div[1]/"
+                                           "div/form/span/section/div/div/div[3]/div[1]/div/div/div[1]/div/div[1]/div/div[1]/input").send_keys(password)
+        # вводим сгенерированный пароль для подтверждения
+        self.driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/div/div/div[2]/div/div[1]/"
+                                           "div/form/span/section/div/div/div[3]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input").send_keys(password)
+        #Нажимаем кнопку далее
+        self.driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/button/span").click()
+        time.sleep(3)  # We use time sleep to give the page enoght time to load
+        birthdate = birthdate.split('.')
+        #Вводим день рождения
+        self.driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/div/div/div[2]/div/div[1]/"
+                                           "div/form/span/section/div/div/div[3]/div[1]/div/div/div[1]/div/div[1]/input").send_keys(birthdate[0])
+        # Вводим месяц рождения
+        self.driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/div/div/div[2]/div/div[1]"
+                                           "/div/form/span/section/div/div/div[3]/div[2]/div/div/div[2]/select").send_keys(birthdate[1])
+        # Вводим год рождения
+        self.driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/div/div/div[2]/div/div[1]"
+                                           "/div/form/span/section/div/div/div[3]/div[2]/div/div/div[2]/select").send_keys(birthdate[1])
+        # Выбираем пол
+        self.driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/div/div/div[2]/div/div[1]"
+                                           "/div/form/span/section/div/div/div[4]/div[1]/div/div[2]/select").send_keys(s)
+        # Нажимаем кнопку далее
+        self.driver.find_element(By.XPATH,"/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/button/span").click()
+        time.sleep(3)  # We use time sleep to give the page enoght time to load
+        # Принимаем условия конфиденциальности
+        self.driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/button/div[3]").click()
+        time.sleep(3)  # We use time sleep to give the page enoght time to load
 
     def login(self, email, password):
         """ Login function sigh in googlemail with email and password as arguments"""
@@ -20,7 +63,7 @@ class Automail:
         self.driver.find_element(By.NAME, "password").send_keys(password)
         self.driver.find_element(By.ID, "passwordNext").click()
 
-    def mail(self, email, theame, context):
+    def send_mail(self, email, theame, context):
         """Mail functions receives email, theames and context for sending """
         self.driver.get('https://mail.google.com')
         time.sleep(3) #We use time sleep to give the page enoght time to load
---
+++ Create_mail_password.py
@@ -0,0 +1,22 @@
+import re
+import random
+
+def create_mail(name,surname,birthdate):
+    birthdate = birthdate.split('.')
+    return f'{name}.{surname}{int(birthdate[0])*random.randint(1,9)}{int(birthdate[1])*random.randint(1,9)}'
+
+def create_password():
+    length = random.randint(10, 20)
+    varchar = list('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_!@#$%^&*()_!@#$%^&*()_!@#$%^&*()_!@#$%^&*()')
+    flag = 'False'
+    while flag != "True":
+        password = ''.join([random.choice(varchar) for _ in range(length)])
+        if re.findall(r'[A-Za-z0-9@#$%^&+=]', password):
+            flag = 'True'
+    return password
+
+#print(create_password())
+#print(create_mail('Anton','Groznyi','12.11.1990'))
+
+
+
