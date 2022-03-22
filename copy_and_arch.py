2e569cf8654cbbe7ca979908ff04ce547bd71897
YarRus12 <97214900+YarRus12@users.noreply.github.com>
2022-03-22 15:56:02 +1000

    New function is added. Need to fibish archive function and remove function

--- copy_and_arch.py
+++ copy_and_arch.py
@@ -1,5 +1,7 @@
 import os
 from shutil import copyfile, copy
+import zipfile
+
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -9,8 +11,14 @@ copyfile(__file__, os.path.join(destdir, filename))  # копируем по п�
 
 
 def copy(scr, dest):
-    shutil.copytree(scr, dest+'/Data/Archives')
+    shutil.copytree(scr, dest+'/Data/Copy')
 
-def archive(from_path, to_path):
-    pass
+def archive(folder, to_path):
+    """Нужно будет доработать"""
+    mail_zip = zipfile.ZipFile(BASE_DIR, 'w')
+    for folder, subfolders, files in os.walk(BASE_DIR):
+        for file in files:
+                mail_zip.write(os.path.join(folder, file), os.path.relpath(os.path.join(folder, file), 'C:\\Stories\\Fantasy'),
+                                  compress_type=zipfile.ZIP_DEFLATED)
 
+    fantasy_zip.close()
