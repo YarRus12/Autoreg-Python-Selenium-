1112f57e4f3eb26ebbca3019ef7f5f6eff9137a0
Yaroslav Russu <yaroslav.russu@mail.ru>
2022-03-24 21:39:29 +1000

    New function is added. Need to fibish archive function and remove function

--- copy_and_arch.py
+++ copy_and_arch.py
@@ -16,9 +16,8 @@ def copy(scr, dest):
 def archive(folder, to_path):
     """Нужно будет доработать"""
     mail_zip = zipfile.ZipFile(BASE_DIR, 'w')
-    for folder, subfolders, files in os.walk(BASE_DIR):
+    for folder, subfolders, files in os.walk(folder):
         for file in files:
-                mail_zip.write(os.path.join(folder, file), os.path.relpath(os.path.join(folder, file), 'C:\\Stories\\Fantasy'),
+                mail_zip.write(os.path.join(folder, file), os.path.relpath(os.path.join(folder, file), to_path),
                                   compress_type=zipfile.ZIP_DEFLATED)
-
-archive(BASE_DIR+"/Data/Copy")
+archive(BASE_DIR+"/Data/Copy", BASE_DIR+"/Data/Archives")
