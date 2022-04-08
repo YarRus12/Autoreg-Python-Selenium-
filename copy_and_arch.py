cb7f50f82b79bad1d7f5cf82312b3f7165f77274
YarRus12 <97214900+YarRus12@users.noreply.github.com>
2022-04-08 22:45:20 +1000

    Усовершенствован механизм копирования.

--- copy_and_arch.py
+++ copy_and_arch.py
@@ -6,7 +6,10 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 def copy_f(scr, dest):
+
     for i in os.listdir(scr):
+        """Проблема в том что паки копия не существует, чтобы ее решить нужно найти фрагмент кода в котором
+        мы проверяем наличие папки и если ее нет, то создаем"""
         try:
             if i.split('.')[1] == 'py':
                 shutil.copy(i, dest+i)
@@ -22,5 +25,5 @@ def archive(folder, to_path):
                 mail_zip.write(os.path.join(folder, file), os.path.relpath(os.path.join(folder, file), to_path),
                                   compress_type=zipfile.ZIP_DEFLATED)
 
-copy_f(BASE_DIR, BASE_DIR+"/Data/Copy")
+copy_f(BASE_DIR, BASE_DIR+"/Data/")
 #archive(BASE_DIR+"/Data/Copy", BASE_DIR+"/Data/Archives")
