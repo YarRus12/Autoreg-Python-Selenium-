2ab7829fc6d741e209448a3aed2c805aad71a6c2
Yaroslav Russu <yaroslav.russu@mail.ru>
2022-04-09 17:12:39 +1000

    Продолжается работа по настройке архиватора

--- copy_and_arch.py
+++ copy_and_arch.py
@@ -5,32 +5,31 @@ import zipfile
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-
 def copy_f(scr, copy_folder):
-    """Нужно будет доработать"""
+    """Функция принимает в себя значение источника и
+    значение папки: куда следует поместить копии файлов"""
+    #Если папка для копий файлов не существует, ее необходимо создать
     if not os.path.exists(copy_folder):
         os.makedirs(copy_folder, exist_ok=True)
+    #В цикле перебираем список файлов в источнике
+    #и если наименования файлов включают в себя расширение .py то копируем их.
     for i in os.listdir(scr):
         try:
             if i.split('.')[1] == 'py':
                 shutil.copy(i, copy_folder)
         except IndexError:
             pass
-
-def trash_f(folder):
-    trash_folder = os.path.join(folder, 'Trash')
-    if not os.path.exists(trash_folder):
-        os.makedirs(trash_folder, exist_ok=True)
+    return copy_folder
 
 def archive(folder, to_path):
     """Осталось архив дописать"""
     copy_folder = os.path.join(to_path, 'Copy')
-    #copy_f(folder, copy_folder)
-    archive_folder = os.path.join(to_path, 'Archives')
-    ziph = zipfile.ZipFile("Archives.zip", 'w')
-    for root, dirs, files in os.walk(to_path):
-        for file in files:
-            ziph.write(os.path.join(copy_folder, file), arcname=archive_folder)
-    ziph.close()
+    copy_folder = copy_f(folder, copy_folder)
+    with zipfile.ZipFile("Archives.zip", mode="w") as archive:
+        for root, dirs, files in os.walk(copy_folder):
+            for file in files:
+                archive.write(file)
+    #Удаляем папку с копиями
+    shutil.rmtree(copy_folder)
 
 archive(BASE_DIR, BASE_DIR+"/Data")
